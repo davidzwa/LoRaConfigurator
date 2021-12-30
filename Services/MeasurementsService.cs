ca8e614a2ba5d8b2e659fe9f6a2ad4ce7b929b5f
David Zwart <davidzwa@gmail.com>
2021-12-30 10:56:27 +0100

    Keep file lock (#8)

--- Services/MeasurementsService.cs
+++ Services/MeasurementsService.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 
 namespace LoraGateway.Services;
 
@@ -10,59 +11,85 @@ public class MeasurementDto
     public int Rssi { get; set; }
 }
 
-public class MeasurementsService
+public class MeasurementsService : IDisposable
 {
     private readonly ILogger<MeasurementsService> _logger;
-    private List<MeasurementDto> _measurementDtos = new();
-    private String _location = "";
-    
+    private string _location = "";
+    private readonly List<MeasurementDto> _measurementDtos = new();
+
+    private FileStream? _measurementFile;
+
     public MeasurementsService(ILogger<MeasurementsService> logger)
     {
         _logger = logger;
     }
+
+    public void Dispose()
+    {
+        _measurementFile?.Close();
+    }
+
     public string GetMeasurementFile()
     {
         return Path.GetFullPath($"../../../Data/measurements{_location}.json", Directory.GetCurrentDirectory());
     }
-    
 
-    private async Task EnsureSourceExists()
+    private void EnsureSourceExists()
     {
         var path = GetMeasurementFile();
         var dirName = Path.GetDirectoryName(path);
         if (dirName == null) return;
 
         if (!Directory.Exists(dirName)) Directory.CreateDirectory(dirName);
-        // if (!File.Exists(path))
-        // {
-        //     await File.WriteAllLinesAsync(path, null);
-        // }
     }
-    
-    public async Task InitMeasurements()
+
+    public void InitMeasurements()
     {
-        this._measurementDtos.Clear();
-        await EnsureSourceExists();
+        _measurementDtos.Clear();
+        EnsureSourceExists();
     }
 
     public void SetLocation(int x, int y)
     {
         _location = $"x{x}_y{y}";
+        UpdateFileLock();
     }
-    
+
+    private void UpdateFileLock()
+    {
+        CloseFileIfOpened();
+        if (string.IsNullOrEmpty(_location)) return;
+        OpenFile(GetMeasurementFile());
+    }
+
+    private void CloseFileIfOpened()
+    {
+        _measurementFile?.Close();
+        _measurementFile = null;
+    }
+
+    private void OpenFile(string path)
+    {
+        CloseFileIfOpened();
+        _measurementFile = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+    }
+
     public void SetLocationText(string locationText)
     {
         _location = locationText.Trim().Replace(" ", "_");
+
+        UpdateFileLock();
     }
 
     public async Task AddMeasurement(uint seq, uint snr, int rssi)
     {
-        if (String.IsNullOrEmpty(_location))
+        if (string.IsNullOrEmpty(_location))
         {
             _logger.LogInformation("Skipped measurement as location was unset. SeqNr:{Seq}", seq);
             return;
         }
-        _measurementDtos.Add(new()
+
+        _measurementDtos.Add(new MeasurementDto
         {
             TimeStamp = DateTimeOffset.Now.ToUnixTimeSeconds(),
             Rssi = rssi,
@@ -70,8 +97,10 @@ public class MeasurementsService
             SequenceNumber = seq
         });
 
-        var jsonBlob = JsonSerializer.Serialize(_measurementDtos, new JsonSerializerOptions() {WriteIndented = true});
-        
-        await File.WriteAllTextAsync(GetMeasurementFile(), jsonBlob);
+        var jsonBlob = JsonSerializer.Serialize(_measurementDtos, new JsonSerializerOptions { WriteIndented = true });
+        var blob = Encoding.UTF8.GetBytes(jsonBlob);
+        if (_measurementFile == null) OpenFile(GetMeasurementFile());
+
+        await _measurementFile.WriteAsync(blob);
     }
 }
