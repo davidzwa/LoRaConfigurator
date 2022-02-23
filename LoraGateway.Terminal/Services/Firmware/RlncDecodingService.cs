5ae3d15d0e825a18a88fea22801eb2d30443a9e1
Unknown <davidzwa@gmail.com>
2022-02-23 18:36:29 +0100

    Add RLNC decoding rank determination service

---
+++ LoraGateway.Terminal/Services/Firmware/RlncDecodingService.cs
@@ -0,0 +1,14 @@
+﻿using LoraGateway.Services.Firmware.RandomLinearCoding;
+
+namespace LoraGateway.Services.Firmware;
+
+public class RlncDecodingService
+{
+    public 
+    public uint DetermineEncodingMatrixRank(List<IPacket> packets)
+    {
+        // Rows should at least exceed col counts
+
+        return 0;
+    }
+}
