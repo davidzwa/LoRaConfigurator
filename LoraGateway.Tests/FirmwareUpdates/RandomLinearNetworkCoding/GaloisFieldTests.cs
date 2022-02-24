a325da079fac0603480fc2b6be23ca5927a40ec4
Unknown <davidzwa@gmail.com>
2022-02-24 13:26:56 +0100

    Test pivot inversion procedure

--- LoraGateway.Tests/FirmwareUpdates/RandomLinearNetworkCoding/GaloisFieldTests.cs
+++ LoraGateway.Tests/FirmwareUpdates/RandomLinearNetworkCoding/GaloisFieldTests.cs
@@ -7,7 +7,7 @@ namespace LoraGateway.Tests.FirmwareUpdates;
 public class GaloisFieldTests
 {
     [Fact]
-    public void TestGaloisFieldOperations()
+    public void GaloisFieldOperationsTest()
     {
         // Verified with:
         // https://asecuritysite.com/encryption/gf?a0=1%2C0%2C0%2C1&a1=1%2C0%2C1&b0=1%2C0%2C0%2C1%2C1
@@ -18,5 +18,20 @@ public class GaloisFieldTests
         (fieldA + fieldB).GetValue().ShouldBe((byte)0x07);
         (fieldA - fieldB).GetValue().ShouldBe((byte)0x07);
         (fieldA * fieldB).GetValue().ShouldBe((byte)0b1100);
+    }
+
+    [Fact]
+    public void GaloisFieldInversionTest()
+    {
+        var a = new GField(133);
+        var b = new GField(217);
+        var unit = new GField(1);
+
+        var aInverse = unit / a;
+        (a * aInverse).ShouldBe(unit);
+
+        var c = a * b;
+        (c / b).ShouldBe(a);
+        GField.Log.Length.ShouldBe(256);
     }
 }
