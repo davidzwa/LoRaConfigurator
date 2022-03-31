589a0d7a5d5ca352652fa98f1e077984b605ffcc
Unknown <davidzwa@gmail.com>
2022-03-31 14:00:29 +0200

    Make Rssi and Snr average window config something for later

--- LoraGateway.Terminal/Models/ExperimentConfig.cs
+++ LoraGateway.Terminal/Models/ExperimentConfig.cs
@@ -5,8 +5,11 @@ public class ExperimentConfig : ICloneable
     public float MinPer { get; set; } = 0.05f;
     public float MaxPer { get; set; } = 0.50f;
     public float PerStep { get; set; } = 0.05f;
-    private int RssiAvgWindow { get; set; } = 10;
-    private int SnrAvgWindow { get; set; } = 10;
+    
+    // Not implemented yet
+    // private int RssiAvgWindow { get; set; } = 10;
+    // Not implemented yet
+    // private int SnrAvgWindow { get; set; } = 10;
 
     public bool AwaitUartAck { get; set; } = false;
     public int ExperimentTimeout = 30000;
