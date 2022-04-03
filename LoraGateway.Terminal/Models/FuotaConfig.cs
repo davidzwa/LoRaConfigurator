0de4eca919ed061f42e40b067063d5370b46b886
David Zwart <davidzwa@gmail.com>
2022-04-03 17:52:42 +0200

    Simplify and reuse generation loss detection at end of experiment run

--- LoraGateway.Terminal/Models/FuotaConfig.cs
+++ LoraGateway.Terminal/Models/FuotaConfig.cs
@@ -4,13 +4,18 @@ namespace LoraGateway.Models;
 
 public class FuotaConfig : ICloneable
 {
+    public uint GetGenerationCount()
+    {
+        return (uint)Math.Ceiling((float)FakeFragmentCount / GenerationSize);
+    }
+
     // Phy settings
     public int TxPower { get; set; } = 14;
     public uint TxBandwidth { get; set; } = 2; // 0(125k),1(250k),2(500k)
     public uint TxDataRate { get; set; } = 11; // 7-12
     public uint SeqPeriodMs { get; set; } = 2000;
     public uint SeqCount { get; set; } = 10;
-    
+
     // Enabling this makes sure the UART packet are treated are LoRa RX packets instead of forwarding them
     public bool UartFakeLoRaRxMode { get; set; } = true;
     public bool UartFakeAwaitAck { get; set; } = false;
@@ -25,25 +30,30 @@ public class FuotaConfig : ICloneable
     public UInt32 PacketErrorSeed { get; set; } = 1963;
     public uint LocalUpdateIntervalMs { get; set; } = 500;
     public bool DebugMatrixUart { get; set; } = false;
+
     public bool DebugFragmentUart { get; set; } = false;
+
     // Not checked
     public bool FakeFirmware { get; set; } = true;
+
     // Not implemented
     // public string? FirmwareBinPath { get; set; } = null;
     // Used for fake generation
     public uint FakeFragmentCount { get; set; } = 10;
+
     // Used for fake generation
     public uint FakeFragmentSize { get; set; } = 10;
+
     // With all other params will result in GenerationCount
     public uint GenerationSize { get; set; } = 5;
     public uint GenerationSizeRedundancy { get; set; } = 2;
-    
+
     // Encoding related
     // Changing poly of Field is not implemented
     // public int FieldPoly { get; set; }
     // Static degree, might become dynamic later
     public uint FieldDegree { get; set; } = 8;
-    
+
     // Changing poly of LFSR is not seemingly interesting
     // public int LfsrPoly { get; set; }
     // Static seed might change to dynamic later
--- LoraGateway.Terminal/Services/ExperimentService.cs
+++ LoraGateway.Terminal/Services/ExperimentService.cs
@@ -111,11 +111,81 @@ public class ExperimentService : JsonDataStore<ExperimentConfig>
         }
     }
 
+
+    public async Task RunExperiments()
+    {
+        var experimentConfig = await LoadStore();
+
+        _dataPoints = new();
+        var min = experimentConfig.MinPer;
+        var max = experimentConfig.MaxPer;
+        var step = experimentConfig.PerStep;
+
+        _cancellationTokenSource = new CancellationTokenSource();
+        _hasCrashed = false;
+
+        var per = min;
+        while (per < max && !_hasCrashed)
+        {
+            var result = _cancellationTokenSource.TryReset();
+            _decodingUpdatesReceived.Clear();
+            _lastResultGenerationIndex = null;
+
+            _logger.LogInformation("Next gen started. CT: {CT}", result);
+            _fuotaManagerService.SetPacketErrorRate(per);
+            _currentPer = per;
+
+            if (experimentConfig.RandomPerSeed)
+            {
+                var buffer = new byte[sizeof(Int32)];
+                new Random().NextBytes(buffer);
+                _fuotaManagerService.SetPacketErrorSeed(BitConverter.ToUInt16(buffer.Reverse().ToArray()));
+            }
+
+            var fuotaConfig = _fuotaManagerService.GetStore();
+
+            var loraMessageStart = _fuotaManagerService.RemoteSessionStartCommand();
+            _serialProcessorService.SetDeviceFilter(fuotaConfig.TargetedNickname);
+            _deviceRlncRoundTerminated = false;
+
+            _logger.LogInformation("PER {Per} - Awaiting {Ms} Ms", per, experimentConfig.ExperimentTimeout);
+            _serialProcessorService.SendUnicastTransmitCommand(loraMessageStart, fuotaConfig.UartFakeLoRaRxMode);
+
+            // Await completion            
+            await Task.WhenAny(Task.Delay(experimentConfig.ExperimentTimeout, _cancellationTokenSource.Token),
+                AwaitTermination(_cancellationTokenSource.Token));
+
+            _logger.LogInformation("Await completed");
+
+            var loraMessageStop = _fuotaManagerService.RemoteSessionStopCommand();
+            _serialProcessorService.SendUnicastTransmitCommand(loraMessageStop, fuotaConfig.UartFakeLoRaRxMode);
+
+            per += step;
+            await Task.Delay(500);
+
+            // Check if some generations were not registered
+            var genCount = fuotaConfig.GetGenerationCount();
+            if (_lastResultGenerationIndex != genCount - 1)
+            {
+                // Of course this Generation Index does not really exist - its used as diff
+                var lostIndices = CalculateLostGenerationIndices(genCount);
+                AppendLostGenerationsToDataPoints(lostIndices, genCount);
+            }
+
+            await Task.Delay(2000);
+        }
+
+        ExportPlot();
+
+        _logger.LogInformation("Experiment done");
+        _dataPoints = null;
+    }
+
     private async Task ProcessResultInner(DecodingResult result)
     {
         var currentGenIndex = result.CurrentGenerationIndex;
         var fuotaConfig = _fuotaManagerService.GetStore();
-        var generationPacketsCount = fuotaConfig.GenerationSize + fuotaConfig.GenerationSizeRedundancy;
+        
 
         if (_lastResultGenerationIndex == result.CurrentGenerationIndex)
         {
@@ -123,87 +193,9 @@ public class ExperimentService : JsonDataStore<ExperimentConfig>
         }
 
         // We detect skipped generations which need to be accounted
-        var missedGens = _lastResultGenerationIndex == null
-            ? (int)currentGenIndex
-            : (int)(currentGenIndex - (_lastResultGenerationIndex! + 1));
-        if (_lastResultGenerationIndex == null && currentGenIndex > 0 ||
-            _lastResultGenerationIndex != null && missedGens > 0)
-        {
-            var missedGenerationIndices = new List<uint>();
-            var missedDecodingUpdates = new List<DecodingUpdate>();
-            if (_lastResultGenerationIndex != null && missedGens > 0)
-            {
-                missedGenerationIndices = Enumerable.Range((int)_lastResultGenerationIndex!+1, missedGens).Select(x => (uint)x).ToList();
-                if (missedGenerationIndices.Count != missedGenerationIndices.Distinct().Count())
-                {
-                    throw new InvalidOperationException(
-                        $"Missed dupes in range distinct {missedGenerationIndices.Distinct().Count()} vs range {missedGenerationIndices.Count}");
-                }
-            }
-            else
-            {
-                missedGenerationIndices = Enumerable.Range(0, missedGens).Select(x => (uint)x).ToList();
-                if (missedGenerationIndices.Count != missedGenerationIndices.Distinct().Count())
-                {
-                    throw new InvalidOperationException(
-                        $"Missed dupes in range distinct {missedGenerationIndices.Distinct().Count()} vs range {missedGenerationIndices.Count}");
-                }
-            }
-            
-            var groupedDecodingUpdatesByGenIndex = _decodingUpdatesReceived.GroupBy(d => d.CurrentGenerationIndex);
-            foreach (var index in missedGenerationIndices)
-            {
-                var foundGenUpdates = groupedDecodingUpdatesByGenIndex.FirstOrDefault(d => d.Key == index);
-                var wasLostGeneration = false;
-                if (foundGenUpdates == null)
-                {
-                    foundGenUpdates = new Grouping<uint, DecodingUpdate>(index)
-                    {
-                        new ()
-                        {
-                            RankProgress = 0,
-                            CurrentGenerationIndex = index,
-                            ReceivedFragments = 0,
-                            MissedGenFragments = generationPacketsCount,
-                            IsRunning = false // Sneaky way to indicate this was a passive data point
-                        }
-                    };
-                    wasLostGeneration = true;
-                    _logger.LogWarning("Could not find missed gen update for index {Index} - inserted fake one with 100% loss", index);
-                }
-
-                var maxTotalPackets = foundGenUpdates.Max(g => g.ReceivedFragments + g.MissedGenFragments);
-                var lastGenUpdate = foundGenUpdates.Last();
-                var totalPacketsFound = lastGenUpdate.ReceivedFragments + lastGenUpdate.MissedGenFragments;
-                if (lastGenUpdate.ReceivedFragments + lastGenUpdate.MissedGenFragments != maxTotalPackets)
-                {
-                    throw new Exception($"Last gen update for index {index} with {totalPacketsFound} total was not newest ({maxTotalPackets})");
-                }
-                var total = lastGenUpdate.ReceivedFragments + lastGenUpdate.MissedGenFragments;
-                _logger.LogWarning("Appending loss {MissedGenFragments} out of {TotalFragments}", lastGenUpdate.MissedGenFragments, total);
-                
-                var per = (float)lastGenUpdate.MissedGenFragments / total;
-                _dataPoints.Add(new()
-                {
-                    Rank = 0,
-                    Success = false,
-                    GenerationIndex = lastGenUpdate.CurrentGenerationIndex,
-                    GenerationTotalPackets = total,
-                    GenerationRedundancyUsed = fuotaConfig.GenerationSizeRedundancy,
-                    MissedPackets = lastGenUpdate.MissedGenFragments,
-                    ReceivedPackets = lastGenUpdate.ReceivedFragments,
-                    RngResolution = total,
-                    TriggeredByDecodingResult = false,
-                    TriggeredByCompleteLoss = wasLostGeneration,
-                    PacketErrorRate = per,
-                    ConfiguredPacketErrorRate = _currentPer
-                });
-                missedDecodingUpdates.Add(lastGenUpdate);
-            }
-                
-            _logger.LogWarning(
-                "Missed generations during run - Experiment failure RecvGen:{LastGenUpdate} MissedGens:{MissedGens} PER:{CurrentPer}",
-                currentGenIndex, missedDecodingUpdates.Count(), _currentPer);
+        var lostIndices = CalculateLostGenerationIndices(currentGenIndex);
+        if (lostIndices.Count > 0) {
+            AppendLostGenerationsToDataPoints(lostIndices, currentGenIndex);
         }
 
         // Now set it straight
@@ -264,64 +256,6 @@ public class ExperimentService : JsonDataStore<ExperimentConfig>
         _logger.LogInformation("Cancelled termination");
     }
 
-    public async Task RunExperiments()
-    {
-        var experimentConfig = await LoadStore();
-
-        _dataPoints = new();
-        var min = experimentConfig.MinPer;
-        var max = experimentConfig.MaxPer;
-        var step = experimentConfig.PerStep;
-
-        _cancellationTokenSource = new CancellationTokenSource();
-        _hasCrashed = false;
-
-        var per = min;
-        while (per < max && !_hasCrashed)
-        {
-            var result = _cancellationTokenSource.TryReset();
-            _decodingUpdatesReceived.Clear();
-            _lastResultGenerationIndex = null;
-
-            _logger.LogInformation("Next gen started. CT: {CT}", result);
-            _fuotaManagerService.SetPacketErrorRate(per);
-            _currentPer = per;
-
-            if (experimentConfig.RandomPerSeed)
-            {
-                var buffer = new byte[sizeof(Int32)];
-                new Random().NextBytes(buffer);
-                _fuotaManagerService.SetPacketErrorSeed(BitConverter.ToUInt16(buffer.Reverse().ToArray()));
-            }
-
-            var fuotaConfig = _fuotaManagerService.GetStore();
-
-            var loraMessageStart = _fuotaManagerService.RemoteSessionStartCommand();
-            _serialProcessorService.SetDeviceFilter(fuotaConfig.TargetedNickname);
-            _deviceRlncRoundTerminated = false;
-
-            _logger.LogInformation("PER {Per} - Awaiting {Ms} Ms", per, experimentConfig.ExperimentTimeout);
-            _serialProcessorService.SendUnicastTransmitCommand(loraMessageStart, fuotaConfig.UartFakeLoRaRxMode);
-
-            // Await completion            
-            await Task.WhenAny(Task.Delay(experimentConfig.ExperimentTimeout, _cancellationTokenSource.Token),
-                AwaitTermination(_cancellationTokenSource.Token));
-
-            _logger.LogInformation("Await completed");
-
-            var loraMessageStop = _fuotaManagerService.RemoteSessionStopCommand();
-            _serialProcessorService.SendUnicastTransmitCommand(loraMessageStop, fuotaConfig.UartFakeLoRaRxMode);
-
-            per += step;
-            await Task.Delay(2000);
-        }
-
-        ExportPlot();
-
-        _logger.LogInformation("Experiment done");
-        _dataPoints = null;
-    }
-
     private void ExportPlot()
     {
         if (_dataPoints.Count == 0)
@@ -339,7 +273,7 @@ public class ExperimentService : JsonDataStore<ExperimentConfig>
                 ConfiguredPer = b.Key
             };
         });
-        
+
         _logger.LogInformation("Saving experiment plot");
         var configuredPerArray = perBuckets.Select(p => (double)p.ConfiguredPer).ToArray();
         var avgPerArray = perBuckets.Select(p => (double)p.PerAvg).ToArray();
@@ -350,5 +284,106 @@ public class ExperimentService : JsonDataStore<ExperimentConfig>
         plt.YLabel("Averaged realised PER");
         plt.XLabel("Configured PER");
         plt.SaveFig(GetPlotFilePath());
+    }
+
+    private List<uint> CalculateLostGenerationIndices(uint currentGenIndex)
+    {
+        var missedGenerationIndices = new List<uint>();
+        var missedGens = _lastResultGenerationIndex == null
+            ? (int)currentGenIndex
+            : (int)(currentGenIndex - (_lastResultGenerationIndex! + 1));
+        if (_lastResultGenerationIndex == null && currentGenIndex > 0 ||
+            _lastResultGenerationIndex != null && missedGens > 0)
+        {
+            if (_lastResultGenerationIndex != null && missedGens > 0)
+            {
+                missedGenerationIndices = Enumerable.Range((int)_lastResultGenerationIndex! + 1, missedGens)
+                    .Select(x => (uint)x).ToList();
+                if (missedGenerationIndices.Count != missedGenerationIndices.Distinct().Count())
+                {
+                    throw new InvalidOperationException(
+                        $"Missed dupes in range distinct {missedGenerationIndices.Distinct().Count()} vs range {missedGenerationIndices.Count}");
+                }
+            }
+            else
+            {
+                missedGenerationIndices = Enumerable.Range(0, missedGens).Select(x => (uint)x).ToList();
+                if (missedGenerationIndices.Count != missedGenerationIndices.Distinct().Count())
+                {
+                    throw new InvalidOperationException(
+                        $"Missed dupes in range distinct {missedGenerationIndices.Distinct().Count()} vs range {missedGenerationIndices.Count}");
+                }
+            }
+        }
+
+        return missedGenerationIndices;
+    }
+        
+    private List<DecodingUpdate> AppendLostGenerationsToDataPoints(List<uint> missedGenerationIndices, uint? currentGenIndex)
+    {
+        var missedDecodingUpdates = new List<DecodingUpdate>();
+        var fuotaConfig = _fuotaManagerService.GetStore();
+        var generationPacketsCount = fuotaConfig.GenerationSize + fuotaConfig.GenerationSizeRedundancy;
+        
+        var groupedDecodingUpdatesByGenIndex = _decodingUpdatesReceived.GroupBy(d => d.CurrentGenerationIndex);
+        foreach (var index in missedGenerationIndices)
+        {
+            var foundGenUpdates = groupedDecodingUpdatesByGenIndex.FirstOrDefault(d => d.Key == index);
+            var wasLostGeneration = false;
+            if (foundGenUpdates == null)
+            {
+                foundGenUpdates = new Grouping<uint, DecodingUpdate>(index)
+                {
+                    new()
+                    {
+                        RankProgress = 0,
+                        CurrentGenerationIndex = index,
+                        ReceivedFragments = 0,
+                        MissedGenFragments = generationPacketsCount
+                    }
+                };
+                wasLostGeneration = true;
+                _logger.LogWarning(
+                    "Could not find missed gen update for index {Index} - inserted fake one with 100% loss", index);
+            }
+
+            var maxTotalPackets = foundGenUpdates.Max(g => g.ReceivedFragments + g.MissedGenFragments);
+            var lastGenUpdate = foundGenUpdates.Last();
+            var totalPacketsFound = lastGenUpdate.ReceivedFragments + lastGenUpdate.MissedGenFragments;
+            if (lastGenUpdate.ReceivedFragments + lastGenUpdate.MissedGenFragments != maxTotalPackets)
+            {
+                throw new Exception(
+                    $"Last gen update for index {index} with {totalPacketsFound} total was not newest ({maxTotalPackets})");
+            }
+
+            var total = lastGenUpdate.ReceivedFragments + lastGenUpdate.MissedGenFragments;
+            _logger.LogWarning("Appending loss {MissedGenFragments} out of {TotalFragments}",
+                lastGenUpdate.MissedGenFragments, total);
+
+            var per = (float)lastGenUpdate.MissedGenFragments / total;
+            _dataPoints.Add(new()
+            {
+                Rank = 0,
+                Success = false,
+                GenerationIndex = lastGenUpdate.CurrentGenerationIndex,
+                GenerationTotalPackets = total,
+                GenerationRedundancyUsed = fuotaConfig.GenerationSizeRedundancy,
+                MissedPackets = lastGenUpdate.MissedGenFragments,
+                ReceivedPackets = lastGenUpdate.ReceivedFragments,
+                RngResolution = total,
+                TriggeredByDecodingResult = false,
+                TriggeredByCompleteLoss = wasLostGeneration,
+                PacketErrorRate = per,
+                ConfiguredPacketErrorRate = _currentPer
+            });
+            missedDecodingUpdates.Add(lastGenUpdate);
+        }
+
+        var genIndexString = currentGenIndex != null ? currentGenIndex.ToString() : "FINAL";
+        _logger.LogWarning(
+            "Missed generations during run - Experiment failure RecvGen:{LastGenUpdate} MissedGens:{MissedGens} PER:{CurrentPer}",
+            genIndexString, missedDecodingUpdates.Count(), _currentPer);
+
+        return missedDecodingUpdates;
     }
 }
