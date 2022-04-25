d449a0233bc768912bf909576d4b8355ff1329b9
David Zwart <davidzwa@gmail.com>
2022-04-25 02:02:41 +0200

    Adjust config and add event handler for phy experiment

---
+++ LoraGateway.Terminal/Handlers/ExperimentEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using JKang.EventBus;
+using LoraGateway.Services;
+
+namespace LoraGateway.Handlers;
+
+public class ExperimentEventHandler : IEventHandler<RxEvent>, IEventHandler<PeriodTxEvent>
+{
+    private readonly ExperimentPhyService _experimentPhyService;
+
+    public ExperimentEventHandler(
+        ExperimentPhyService experimentPhyService
+    )
+    {
+        _experimentPhyService = experimentPhyService;
+    }
+
+    public async Task HandleEventAsync(RxEvent @event)
+    {
+        await _experimentPhyService.ReceiveMessage(@event);
+    }
+
+    public async Task HandleEventAsync(PeriodTxEvent @event)
+    {
+        await _experimentPhyService.ReceivePeriodTxMessage(@event);
+    }
+}
